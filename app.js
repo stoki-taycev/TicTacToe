d0e414291a7261046ddaed73188907996d45c530
Anton Udyrenko <stokipost@gmail.com>
2015-02-17 13:26:02 +0200

    AI logic finished

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ myApp.controller('SpicyController', ['$scope', '$timeout', function ($scope, $ti
 
 
 
+  // Player`s turn with chain of gameplay methods: AI turn and winner check
+
   $scope.makeTurn = function(id) {
 
     if ($scope['mark' + id] === null) {
@@ -28,8 +30,8 @@ myApp.controller('SpicyController', ['$scope', '$timeout', function ($scope, $ti
     $timeout($scope.appTurn, 1000);
 
     if (($scope.marker != null) && ($scope.appMarker != null)) {
-      $scope.winCheck($scope.marker);
-      $scope.winCheck($scope.appMarker);
+      $scope.isWinner($scope.marker);
+      $scope.isWinner($scope.appMarker);
     };
 
     if ($scope.win != "win") {
@@ -39,7 +41,7 @@ myApp.controller('SpicyController', ['$scope', '$timeout', function ($scope, $ti
 
 
 
-  //
+  // AI logic
 
   $scope.appTurn = function() {
 
@@ -50,7 +52,7 @@ myApp.controller('SpicyController', ['$scope', '$timeout', function ($scope, $ti
       $scope.appMarker = $scope.markerX;
     };
 
-    // AI algorythm
+    // AI turn algorythm
     if ($scope.mark5 === null) {
       $scope.mark5 = $scope.appMarker;
     }else if (($scope.diagonalMarks.mark1 === null) &&
@@ -59,55 +61,103 @@ myApp.controller('SpicyController', ['$scope', '$timeout', function ($scope, $ti
               ($scope.diagonalMarks.mark9 === null)
       ) {
       randomMove($scope.diagonalMarks, $scope.diagonalMarksIndexes);
-    }else if ($scope.checkDefence($scope.marker)) { // I should create a new method and insert it`s call here - checkWin for higher priority (checkDefence && winCheck and below just checkDefence)
+    }else if (($scope.checkWinOrDefence($scope.marker)) && ($scope.checkWinOrDefence($scope.appMarker))) {
+      $scope.makeWin($scope.appMarker);
+    }else if ($scope.checkWinOrDefence($scope.marker)) {
       $scope.makeDefence($scope.marker, $scope.appMarker);
-    }else if ($scope.checkDefence($scope.marker) != true) {
+    }else if ($scope.checkWinOrDefence($scope.marker) != true) {
       randomMove($scope.crossMarks, $scope.crossMarksIndexes);
     }else {
-      $scope.winChance($scope.appMarker);
+      $scope.makeWin($scope.appMarker);
     };
-
-
     console.log("app turned!");
   };
 
 
 
-  // Heuristic random move
+  // Random move - to make AI unpredicted in its villain logic :)
 
   function randomMove(obj, arr) {
     var randomIndex = arr[Math.floor(Math.random()*arr.length)];
     if (obj['mark' + randomIndex] == null) {
-          obj['mark' + randomIndex] = $scope.appMarker;
-        } else {
-          randomMove(obj, arr);
-        };
+      obj['mark' + randomIndex] = $scope.appMarker;
+    } else {
+      randomMove(obj, arr);
+    };
   };
 
-  function isBusy (obj) {
-
-  }
 
 
+   // Check the need to make win turn or defence turn
 
-  // Check winning combinations
-
-  $scope.winCheck = function(marker) {
-    if (
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === marker)) ||
-      (($scope.crossMarks.mark4 === marker) && ($scope.mark5 === marker) && ($scope.crossMarks.mark6 === $scope.marker)) ||
-      (($scope.diagonalMarks.mark7 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark9 === marker)) ||
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === marker)) ||
-      (($scope.crossMarks.mark2 === marker) && ($scope.mark5 === marker) && ($scope.crossMarks.mark8 === marker)) ||
-      (($scope.diagonalMarks.mark3 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark9 === marker)) ||
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.mark5 === marker) && ($scope.diagonalMarks.mark9 === marker)) ||
-      (($scope.diagonalMarks.mark3 === marker) && ($scope.mark5 === marker) && ($scope.diagonalMarks.mark7 === marker))
+  $scope.checkWinOrDefence = function(marker) {
+    if ( // mark1 check:
+      (($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark1 === null)) ||
+      (($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark1 === null)) ||
+      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark1 === null))
     ) {
-        $scope.win = marker + " win!";
+        return true;
+      }
+    else if ( // mark2 check:
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.crossMarks.mark2 === null)) ||
+      (($scope.mark5 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.crossMarks.mark2 === null))
+    ) {
+        return true;
+      }
+    else if ( // mark3 check:
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === null)) ||
+      (($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark3 === null)) ||
+      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark3 === null))
+    ) {
+        return true;
+      }
+    else if ( // mark4 check:
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.crossMarks.mark4 === null)) ||
+      (($scope.mark5 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.crossMarks.mark4 === null))
+    ) {
+        return true;
+      }
+    else if ( // mark5 check:
+      (($scope.crossMarks.mark2 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.mark5 === null)) ||
+      (($scope.crossMarks.mark4 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.mark5 === null)) ||
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.mark5 === null)) ||
+      (($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.mark5 === null))
+    ) {
+        return true;
+      }
+    else if ( // mark6 check:
+      (($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.crossMarks.mark6 === null)) ||
+      (($scope.mark5 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.crossMarks.mark6 === null))
+    ) {
+        return true;
+      }
+    else if ( // mark7 check:
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === null)) ||
+      (($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark7 === null)) ||
+      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark7 === null))
+    ) {
+        return true;
+      }
+    else if ( // mark8 check:
+      (($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.crossMarks.mark8 === null)) ||
+      (($scope.mark5 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.crossMarks.mark8 === null))
+    ) {
+        return true;
+      }
+    else if ( // mark9 check:
+      (($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark9 === null)) ||
+      (($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark9 === null)) ||
+      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark9 === null))
+    ) {
+        return true;
       };
   };
 
-  $scope.winChance = function(marker) {
+
+
+   // Make turn to win the game
+
+  $scope.makeWin = function(marker) {
 
     if ( // mark1 chances:
       (($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark1 === null)) ||
@@ -172,134 +222,92 @@ myApp.controller('SpicyController', ['$scope', '$timeout', function ($scope, $ti
   };
 
 
-  $scope.checkDefence = function(marker) {
 
-    if ( // mark1 chances:
+   // Make turn to defence.
+
+  $scope.makeDefence = function(marker, appMarker) {
+
+    if ( // mark1 defence:
       (($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark1 === null)) ||
       (($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark1 === null)) ||
       (($scope.mark5 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark1 === null))
     ) {
-        return true;
+        $scope.diagonalMarks.mark1 = appMarker;
       }
-    else if ( // mark2 chances:
+    else if ( // mark2 defence:
       (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.crossMarks.mark2 === null)) ||
       (($scope.mark5 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.crossMarks.mark2 === null))
     ) {
-        return true;
+        $scope.crossMarks.mark2 = appMarker;
       }
-    else if ( // mark3 chances:
+    else if ( // mark3 defence:
       (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === null)) ||
       (($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark3 === null)) ||
       (($scope.mark5 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark3 === null))
     ) {
-        return true;
+        $scope.diagonalMarks.mark3 = appMarker;
       }
-    else if ( // mark4 chances:
+    else if ( // mark4 defence:
       (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.crossMarks.mark4 === null)) ||
       (($scope.mark5 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.crossMarks.mark4 === null))
     ) {
-        return true;
+        $scope.crossMarks.mark4 = appMarker;
       }
-    else if ( // mark5 chances:
+    else if ( // mark5 defence:
       (($scope.crossMarks.mark2 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.mark5 === null)) ||
       (($scope.crossMarks.mark4 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.mark5 === null)) ||
       (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.mark5 === null)) ||
       (($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.mark5 === null))
     ) {
-        return true;
+        $scope.mark5 = appMarker;
       }
-    else if ( // mark6 chances:
+    else if ( // mark6 defence:
       (($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.crossMarks.mark6 === null)) ||
       (($scope.mark5 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.crossMarks.mark6 === null))
     ) {
-        return true;
+        $scope.crossMarks.mark6 = appMarker;
       }
-    else if ( // mark7 chances:
+    else if ( // mark7 defence:
       (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === null)) ||
       (($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark7 === null)) ||
       (($scope.mark5 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark7 === null))
     ) {
-        return true;
+        $scope.diagonalMarks.mark7 = appMarker;
       }
-    else if ( // mark8 chances:
+    else if ( // mark8 defence:
       (($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.crossMarks.mark8 === null)) ||
       (($scope.mark5 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.crossMarks.mark8 === null))
     ) {
-        return true;
+        $scope.crossMarks.mark8 = appMarker;
       }
-    else if ( // mark9 chances:
+    else if ( // mark9 defence:
       (($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark9 === null)) ||
       (($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark9 === null)) ||
       (($scope.mark5 === marker) && ($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark9 === null))
     ) {
-        return true;
+        $scope.diagonalMarks.mark9 = appMarker;
       };
   };
 
 
-  $scope.makeDefence = function(marker, oppositeMarker) {
 
-    if ( // mark1 chances:
-      (($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark1 === null)) ||
-      (($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark1 === null)) ||
-      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark1 === null))
+  // Check winning combinations
+
+  $scope.isWinner = function(marker) {
+    if (
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === marker)) ||
+      (($scope.crossMarks.mark4 === marker) && ($scope.mark5 === marker) && ($scope.crossMarks.mark6 === $scope.marker)) ||
+      (($scope.diagonalMarks.mark7 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark9 === marker)) ||
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === marker)) ||
+      (($scope.crossMarks.mark2 === marker) && ($scope.mark5 === marker) && ($scope.crossMarks.mark8 === marker)) ||
+      (($scope.diagonalMarks.mark3 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark9 === marker)) ||
+      (($scope.diagonalMarks.mark1 === marker) && ($scope.mark5 === marker) && ($scope.diagonalMarks.mark9 === marker)) ||
+      (($scope.diagonalMarks.mark3 === marker) && ($scope.mark5 === marker) && ($scope.diagonalMarks.mark7 === marker))
     ) {
-        $scope.diagonalMarks.mark1 = oppositeMarker;
-      }
-    else if ( // mark2 chances:
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.crossMarks.mark2 === null)) ||
-      (($scope.mark5 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.crossMarks.mark2 === null))
-    ) {
-        $scope.crossMarks.mark2 = oppositeMarker;
-      }
-    else if ( // mark3 chances:
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.diagonalMarks.mark3 === null)) ||
-      (($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark3 === null)) ||
-      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark3 === null))
-    ) {
-        $scope.diagonalMarks.mark3 = oppositeMarker;
-      }
-    else if ( // mark4 chances:
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.crossMarks.mark4 === null)) ||
-      (($scope.mark5 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.crossMarks.mark4 === null))
-    ) {
-        $scope.crossMarks.mark4 = oppositeMarker;
-      }
-    else if ( // mark5 chances:
-      (($scope.crossMarks.mark2 === marker) && ($scope.crossMarks.mark8 === marker) && ($scope.mark5 === null)) ||
-      (($scope.crossMarks.mark4 === marker) && ($scope.crossMarks.mark6 === marker) && ($scope.mark5 === null)) ||
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.mark5 === null)) ||
-      (($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.mark5 === null))
-    ) {
-        $scope.mark5 = oppositeMarker;
-      }
-    else if ( // mark6 chances:
-      (($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.crossMarks.mark6 === null)) ||
-      (($scope.mark5 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.crossMarks.mark6 === null))
-    ) {
-        $scope.crossMarks.mark6 = oppositeMarker;
-      }
-    else if ( // mark7 chances:
-      (($scope.diagonalMarks.mark1 === marker) && ($scope.crossMarks.mark4 === marker) && ($scope.diagonalMarks.mark7 === null)) ||
-      (($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.diagonalMarks.mark7 === null)) ||
-      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark7 === null))
-    ) {
-        $scope.diagonalMarks.mark7 = oppositeMarker;
-      }
-    else if ( // mark8 chances:
-      (($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark9 === marker) && ($scope.crossMarks.mark8 === null)) ||
-      (($scope.mark5 === marker) && ($scope.crossMarks.mark2 === marker) && ($scope.crossMarks.mark8 === null))
-    ) {
-        $scope.crossMarks.mark8 = oppositeMarker;
-      }
-    else if ( // mark9 chances:
-      (($scope.crossMarks.mark8 === marker) && ($scope.diagonalMarks.mark7 === marker) && ($scope.diagonalMarks.mark9 === null)) ||
-      (($scope.crossMarks.mark6 === marker) && ($scope.diagonalMarks.mark3 === marker) && ($scope.diagonalMarks.mark9 === null)) ||
-      (($scope.mark5 === marker) && ($scope.diagonalMarks.mark1 === marker) && ($scope.diagonalMarks.mark9 === null))
-    ) {
-        $scope.diagonalMarks.mark9 = oppositeMarker;
+        $scope.win = marker + " win!";
       };
   };
+
 
 
   // Change player (for perpective of two persons gameplay)
